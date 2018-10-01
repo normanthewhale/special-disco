0ac312c2830e6cfd74863bbcd3f6488636e78917
Kyle Gillett <kylegillett@gmail.com>
2018-10-01 15:47:51 -0600

    added fields to band and members

--- app/models/band.js
+++ app/models/band.js
@@ -1,20 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require( 'mongoose' );
 const Schema = mongoose.Schema;
 
-const BandSchema = new Schema({
-  name: String,
-  members:[{ type: Schema.Types.ObjectId, ref: 'Member' }]
-})
+const BandSchema = new Schema( {
+	name: String,
+	members: [ {
+		type: Schema.Types.ObjectId,
+		ref: 'Member'
+	} ],
+	albums: [],
+	genre: String,
+	origin: String,
+	years_active: String
+	rating: Number
+} )
 
-const MemberSchema = new Schema({
-  name: String,
-  band: {
-    type: Schema.Types.ObjectId,
-    ref: 'Band'
-  }
-})
+const MemberSchema = new Schema( {
+	name: String,
+	band: {
+		type: Schema.Types.ObjectId,
+		ref: 'Band'
+	},
+	origin: String,
+	albums: [],
+	instrument: String,
+	genre: String,
+	rating: Number
+} )
+
+
 
 module.exports = {
-  band: mongoose.model('Band', BandSchema),
-  member: mongoose.model('Member', MemberSchema)
+	band: mongoose.model( 'Band', BandSchema ),
+	member: mongoose.model( 'Member', MemberSchema )
 }
