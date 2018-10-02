6b1ddc40d65ce10550d20cc2eba8cbbaaacdcc20
Olesia <olesya.kovalchuk13@gmail.com>
2018-10-01 20:38:51 -0600

    All errors fixed,table styling needs to be fixed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import './App.css';
 import 'typeface-roboto'
 import Button from '@material-ui/core/Button';
 ///nav stuff below
-import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
-import IconButton from '@material-ui/core/IconButton'
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import MenuIcon from '@material-ui/core/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import Input from '@material-ui/core/Input'
 import Card from '@material-ui/core/Card';
@@ -20,9 +17,9 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
-import TextField from '@material-ui/core/TextField'
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
 
 const pics = {
 	badplus: "https://media.npr.org/assets/img/2014/03/19/badplus_wide-ce010832ad4ecb119280cdd62eb818169ee290f9-s700-c85.jpg"
@@ -32,12 +29,10 @@ class App extends Component {
 	render() {
 		return ( <div className="App">
 
-			<AppBar position="static" color="default" title=" Bandbase">
+			<AppBar position="static" color="default" title="Bandbase">
 				<Toolbar>
-					<IconButton className='menuButton' color="inherit" >
-						<MenuIcon/>
-					</IconButton>
-					<Typography variant="title" color="inherit">
+
+					<Typography variant="title" color="inherit" gutterBottom={false}>
 						Bandbase
 					</Typography>
 					<Button className="Nav-btn" color="inherit">Bands</Button>
@@ -45,21 +40,21 @@ class App extends Component {
 					<Button className="Nav-btn" color="inherit">FAQ</Button>
 					<Button className="Nav-btn-help" color="inherit">Help</Button>
 					<SearchIcon/>
-					<Input placeholder="Search..." disableUnderline="disableUnderline"/>
+					<Input placeholder="Search..." disableUnderline={true}/>
 				</Toolbar>
 			</AppBar>
 
-			<Grid className="rowone" container="container" spacing={16}>
-				<Grid item="item">
+			<Grid className="rowone" container={true} spacing={16}>
+				<Grid >
 					<Paper>
 						<Card className="card">
 							<CardActionArea>
 								<CardMedia style={{
 										height: 0,
-										paddingTop: '56%',
+										paddingTop: '56%'
 									}} className="media" image={pics.badplus} title="The Bad Plus"/>
 								<CardContent>
-									<Typography gutterBottom="gutterBottom" variant="headline" component="h2">
+									<Typography gutterBottom={false} variant="headline" component="h2">
 										The Bad Plus
 									</Typography>
 									<Typography component="p">
@@ -79,42 +74,49 @@ class App extends Component {
 					</Paper>
 				</Grid>
 
-				<Grid className="rowone" item="item">
+				<Grid className="rowone">
 					<Paper className="sidebar">
-						<header className="bandHeader">The Bad Plus</header>
-
 						<Table className="table">
+
+							<TableHead >
+								<TableRow>
+									<TableCell>The Bad Plus</TableCell>
+								</TableRow>
+							</TableHead>
+
 							<TableBody>
-								<TableCell>Years Active</TableCell>
-								<TableCell>2000-present</TableCell>
-							</TableBody>
-							<TableBody>
-								<TableCell>Genre</TableCell>
-								<TableCell>Avantgarde Piano Trio</TableCell>
-							</TableBody>
-							<TableBody>
-								<TableCell>Albums</TableCell>
-								<TableCell>These Are the Vistas, Made Possible, Suspicious Activity?</TableCell>
-							</TableBody>
-							<TableBody>
-								<TableCell>Origin</TableCell>
-								<TableCell>Minneapolis, Minnesota, USA</TableCell>
-							</TableBody>
-							<TableBody>
-								<TableCell>Members</TableCell>
-								<TableCell>Orrin Evans, Reid Anderson, David King</TableCell>
-							</TableBody>
-							<TableBody>
-								<TableCell>Past Members</TableCell>
-								<TableCell>Ethan Iverson</TableCell>
-							</TableBody>
-							<TableBody>
-								<TableCell>Labels</TableCell>
-								<TableCell>HUI, Universal, Do the Math</TableCell>
-							</TableBody>
-							<TableBody>
-								<TableCell>Past Times</TableCell>
-								<TableCell>Covers, Out solo's, epic endings</TableCell>
+								<TableRow>
+									<TableCell>Years Active</TableCell>
+									<TableCell>2000-present</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>Genre</TableCell>
+									<TableCell>Avantgarde Piano Trio</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>Albums</TableCell>
+									<TableCell>These Are the Vistas, Made Possible, Suspicious Activity?</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>Origin</TableCell>
+									<TableCell>Minneapolis, Minnesota, USA</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>Members</TableCell>
+									<TableCell>Orrin Evans, Reid Anderson, David King</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>Past Members</TableCell>
+									<TableCell>Ethan Iverson</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>Labels</TableCell>
+									<TableCell>HUI, Universal, Do the Math</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>Past Times</TableCell>
+									<TableCell>Covers, Out solo's, epic endings</TableCell>
+								</TableRow>
 							</TableBody>
 						</Table>
 					</Paper>
@@ -124,17 +126,16 @@ class App extends Component {
 				Play time i show my fluffy belly but it's a trap! if you pet it i will tear up your hand lick sellotape, purrrrrr. Cry louder at reflection leave fur on owners clothes pretend you want to go out but then don't chase laser for dead stare with ears cocked for present belly, scratch hand when stroked or eat the rubberband. Lick face hiss at owner, pee a lot, and meow repeatedly scratch at fence purrrrrr eat muffins and poutine until owner comes back eat prawns daintily with a claw then lick paws clean wash down prawns with a lap of carnation milk then retire to the warmest spot on the couch to claw at the fabric before taking a catnap yet kitty power. Lick human with sandpaper tongue refuse to come home when humans are going to bed; stay out all night then yowl like i am dying at 4am chirp at birds. Swat at dog meow and walk away, but ask for petting. Leave fur on owners clothes lick butt. Claws in your leg destroy couch as revenge yet nya nya nyan cats are fats i like to pets them they like to meow back fall asleep upside-down scratch. Cats secretly make all the worlds muffins this human feeds me, i should be a god cereal boxes make for five star accommodation . Lick the plastic bag lick the plastic bag scamper. Slap owner's face at 5am until human fills food dish litter kitter kitty litty little kitten big roar roar feed me that box? i can fit in that box this human feeds me, i should be a god.
 			</Paper>
 
-			<grid>
+			<Grid >
 				<Paper className="video">
-					<iframe width="400" height="280" src="https://www.youtube.com/embed/q9hOSZGMXlI" frameborder="5" allow="autoplay; encrypted-media" allowfullscreen="allowfullscreen"></iframe>
-					<iframe width="400" height="280" src="https://www.youtube.com/embed/4_6sP_dCJPY" frameborder="5" allow="autoplay; encrypted-media" allowfullscreen="allowfullscreen"></iframe>
-					<iframe width="400" height="280" src="https://www.youtube.com/embed/_7xdRqAO7T4" frameborder="5" allow="autoplay; encrypted-media" allowfullscreen="allowfullscreen"></iframe>
+					<iframe width="400" height="280" src="https://www.youtube.com/embed/q9hOSZGMXlI" frameBorder="5" allow="autoplay; encrypted-media" allowFullScreen="allowfullscreen" title="video_1"></iframe>
+					<iframe width="400" height="280" src="https://www.youtube.com/embed/4_6sP_dCJPY" frameBorder="5" allow="autoplay; encrypted-media" allowFullScreen="allowfullscreen" title="video_2"></iframe>
+					<iframe width="400" height="280" src="https://www.youtube.com/embed/_7xdRqAO7T4" frameBorder="5" allow="autoplay; encrypted-media" allowFullScreen="allowfullscreen" title="video_3"></iframe>
 				</Paper>
-			</grid>
+			</Grid>
 
 		</div> );
 	}
 }
 
-// ReactDOM.render( < App />, document.querySelector( '#app' ) )
 export default App;
